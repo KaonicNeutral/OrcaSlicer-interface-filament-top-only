b919a9100ae15704b2104be44728daba8979f252
KaonicNeutral <292537745+KaonicNeutral@users.noreply.github.com>
2026-06-16 00:45:15 -0400

    remove unneeded stuff

--- src/libslic3r/ExtrusionEntity.hpp
+++ src/libslic3r/ExtrusionEntity.hpp
@@ -102,15 +102,6 @@ inline bool is_bridge(ExtrusionRole role) {
         || role == erOverhangPerimeter;
 }
 
-inline bool is_support_interface(ExtrusionRole role) {
-    return role == erSupportMaterialInterface;
-}
-
-inline bool is_any_support(ExtrusionRole role) {
-    return role == erSupportMaterial
-        || role == erSupportMaterialInterface;
-}
-
 class ExtrusionEntity
 {
 public:
